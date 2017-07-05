b1de22296102a671ad43eb7d332925c534a04d41
Yoshiyuki Hirano <yhirano@me.com>
2017-07-06 01:44:14 +0900

    Updated Genkan::Engine

--- lib/genkan/engine.rb
+++ lib/genkan/engine.rb
@@ -1,5 +1,13 @@
 module Genkan
   class Engine < ::Rails::Engine
     isolate_namespace Genkan
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
