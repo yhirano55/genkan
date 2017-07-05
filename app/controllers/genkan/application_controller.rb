faa576fc789c2934e111b74569b663c9fe300fa5
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-07-05 17:13:08 +0900

    Updated Genkan::ApplicationController

--- app/controllers/genkan/application_controller.rb
+++ app/controllers/genkan/application_controller.rb
@@ -1,5 +1,7 @@
 module Genkan
   class ApplicationController < ActionController::Base
+    include ::Genkan::Authenticatable
     protect_from_forgery with: :exception
+    layout "layouts/application"
   end
 end
