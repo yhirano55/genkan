3da15465874fc73cd7102e0c2b6f7f084d469b82
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2017-07-05 17:17:01 +0900

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Genkan::Engine.routes.draw do
+  get  "/login",                   to: "sessions#new"
+  get  "/logout",                  to: "sessions#destroy"
+  get  "/auth/:provider/callback", to: "sessions#create"
+  post "/auth/:provider/callback", to: "sessions#create"
 end
