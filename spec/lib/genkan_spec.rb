618e50448c14d3510baaae83823eead36f704a21
Yoshiyuki Hirano <yhirano@me.com>
2017-07-06 11:30:09 +0900

    Added lib spec

---
+++ spec/lib/genkan_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Genkan do
+  describe '.configure' do
+    example do
+      expect {
+        described_class.configure do |config|
+          config.user_class_name = "User"
+        end
+      }.not_to raise_error
+    end
+  end
+
+  describe '.config' do
+    subject { described_class.config }
+    it { is_expected.to be_an_instance_of Genkan::Config }
+  end
+
+  describe 'version' do
+    subject { described_class::VERSION }
+    it { is_expected.to be_a String }
+  end
+end
